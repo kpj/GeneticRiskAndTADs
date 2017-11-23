0ba2ddb3390fc3dd2ede4ac58b6340393af4c54e
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2017-11-23 15:37:45 +0100

    Load non-TAD enriched diseases

--- data_reader.py
+++ data_reader.py
@@ -15,10 +15,16 @@ def load_gwas(
 def load_snp_data(snp_dir: str = 'data/snps') -> pd.DataFrame:
     kwargs = dict(header=None, names=[
         'EFO_term', 'disease_name', 'SNP_name', 'chromosome', 'position'])
-    df_cancer_tad = pd.read_table(f'{snp_dir}/dSNPs_cancer_tad.dat', **kwargs)
-    df_cancer_notad = pd.read_table(f'{snp_dir}/dSNPs_cancer.dat', **kwargs)
-    df_nocancer_tad = pd.read_table(f'{snp_dir}/dSNPs_noncancer_tad.dat', **kwargs)
-    df_nocancer_notad = pd.read_table(f'{snp_dir}/dSNPs_noncancer.dat', **kwargs)
+
+    # load all SNP-disease associations
+    df_cancer_tad = pd.read_table(
+        f'{snp_dir}/all/dSNPs_cancer_tad2.dat', **kwargs)
+    df_cancer_notad = pd.read_table(
+        f'{snp_dir}/all/dSNPs_cancer2.dat', **kwargs)
+    df_nocancer_tad = pd.read_table(
+        f'{snp_dir}/all/dSNPs_noncancer_tad2.dat', **kwargs)
+    df_nocancer_notad = pd.read_table(
+        f'{snp_dir}/all/dSNPs_noncancer2.dat', **kwargs)
 
     df_cancer_tad['is_cancer'] = True
     df_cancer_tad['is_tad'] = True
@@ -29,9 +35,23 @@ def load_snp_data(snp_dir: str = 'data/snps') -> pd.DataFrame:
     df_nocancer_notad['is_cancer'] = False
     df_nocancer_notad['is_tad'] = False
 
-    return pd.concat([
+    df_all = pd.concat([
         df_cancer_tad, df_cancer_notad,
         df_nocancer_tad, df_nocancer_notad], axis=0)
+
+    # load associations of TAD-border enriched diseases
+    df_enr_tad = pd.read_table(
+        f'{snp_dir}/tad_enr/dSNPs_all_tad.dat', **kwargs)
+    df_enr_notad = pd.read_table(
+        f'{snp_dir}/tad_enr/dSNPs_all.dat', **kwargs)
+    df_all_enr = pd.concat([df_enr_tad, df_enr_notad], axis=0)
+
+    # mark diseases with TAD border enrichment
+    enr_snps = set(df_all_enr.SNP_name.unique())
+    df_all['disease_tad_enriched'] = df_all['SNP_name'].apply(
+        lambda x: x in enr_snps)
+
+    return df_all
 
 def load_biogrid(
     fname: str = 'data/BIOGRID-ORGANISM-Homo_sapiens-3.4.153.tab2.txt'
@@ -52,3 +72,11 @@ def load_biogrid(
     graph_bgrid = max(nx.connected_component_subgraphs(graph_bgrid_all), key=len)
 
     return graph_bgrid, df_bgrid_phys
+
+
+def main():
+    df = load_snp_data()
+    import ipdb; ipdb.set_trace()
+
+if __name__ == '__main__':
+    main()
