69fed19fc2446a5723df8c9ead77d099bca69eb6
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2018-06-25 23:09:16 +0200

    Fix GWAS-catalog disease splitting

--- utils.py
+++ utils.py
@@ -1,6 +1,28 @@
 import yaml
 
+import pandas as pd
+
 
 def load_config(fname='config.yaml'):
     with open(fname) as fd:
         return yaml.load(fd)
+
+
+def split_df_row(df, column, sep=','):
+    """
+    split_df_row(pd.DataFrame({
+        'mappi': ['a,b', 'c', 'd,e'],
+        'ha': ['hui', 'bui', 'a'],
+        'foo': [1,2,3]
+    }), 'mappi', ',')
+    """
+    def splitter(row, row_accumulator, column):
+        split_row = row[column].split(sep)
+        for s in split_row:
+            new_row = row.to_dict()
+            new_row[column] = s.strip()
+            row_accumulator.append(new_row)
+
+    new_rows = []
+    df.apply(splitter, axis=1, args=(new_rows, column))
+    return pd.DataFrame(new_rows, columns=df.columns)
