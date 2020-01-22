4f638134a18a9f1006564eac99b6b5f7112eddf4
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-01-22 10:31:22 +0100

    Skip Hi-C heatmap plot in test case

--- test_case/run.sh
+++ test_case/run.sh
@@ -8,6 +8,9 @@ rm -rf pipeline_test
 mkdir -p pipeline_test/tads/
 cp -v data/dummy_tads.csv pipeline_test/tads/tads.dummy.42.csv
 
+mkdir -p pipeline_test/hic_files/plots/dummy/
+cp -v data/empty_hic_heatmap.pdf pipeline_test/hic_files/plots/dummy/heatmap.chr1.pdf
+
 # execute pipeline
 cd ..
 snakemake -pr --configfile "test_case/config_dummy.yaml" --use-conda
