6e48e02b7c42dc4974f3335854243ef17f52f79e
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-10-16 11:02:06 +0200

    Plot DAG for test case

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,10 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 # setup environment
 rm -rf pipeline_test
+cd ..
+
+# plot pipeline overview
+snakemake --configfile "tests/config_dummy.yaml" --forceall --dag | dot -Tpdf > tests/dag_test.pdf
 
 # execute pipeline
-cd ..
-snakemake -pr --configfile "tests/config_dummy.yaml" --use-conda
+snakemake -pr --configfile "tests/config_dummy.yaml" -j 1 --use-conda
