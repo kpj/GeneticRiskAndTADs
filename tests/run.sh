f02e672e13c6c74e496bf8164bf110b749cc5f3b
kpj <kim.philipp.jablonski@gmail.com>
2021-02-22 11:51:58 +0100

    Plot all workflow summary networks in test case

--- tests/run.sh
+++ tests/run.sh
@@ -6,8 +6,10 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 rm -rf pipeline_test
 cd ..
 
-# plot pipeline overview
-snakemake --config configfile="tests/config_dummy.yaml" --forceall --dag | dot -Tpdf > tests/dag_test.pdf
+# plot pipeline overview(s)
+for graph_type in dag rulegraph filegraph; do
+    snakemake --config configfile="tests/config_dummy.yaml" --forceall --$graph_type | dot -Tpdf > "tests/test_$graph_type.pdf"
+done
 
 # execute pipeline
 snakemake -pr --config configfile="tests/config_dummy.yaml" -j 1 --use-conda "$@"
