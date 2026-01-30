3f14b3d2ca1cb50a372b774438368635abd36e9f
kpj <kim.philipp.jablonski@gmail.com>
2026-01-30 15:19:06 -0500

    Handle missing genome versions in cooler files

--- workflow/scripts/gather_input_information.py
+++ workflow/scripts/gather_input_information.py
@@ -2,7 +2,33 @@ import cooler
 import pandas as pd
 
 
-def main(fname_list, samplename_list, fname_out):
+def get_genome_version(samplename, genome_version_by_sample_name, cooler_info):
+    implicit_genome_version = cooler_info["genome-assembly"]
+
+    has_explicit_genome_version = samplename in genome_version_by_sample_name
+    explicit_genome_version = genome_version_by_sample_name.get(samplename, "undefined")
+
+    if implicit_genome_version == "unknown" and has_explicit_genome_version:
+        return explicit_genome_version
+
+    if implicit_genome_version != "unknown" and has_explicit_genome_version:
+        print(
+            f"Warning: Both implicit ({implicit_genome_version}) and explicit ({explicit_genome_version}) genome versions are available for sample {samplename}. Using explicit version."
+        )
+        return explicit_genome_version
+
+    if implicit_genome_version == "unknown" and not has_explicit_genome_version:
+        raise ValueError(
+            f"Error: No genome version information available for sample {samplename}."
+        )
+
+    if implicit_genome_version != "unknown" and not has_explicit_genome_version:
+        return implicit_genome_version
+
+    raise RuntimeError("Unreachable code reached in get_genome_version.")
+
+
+def main(fname_list, samplename_list, genome_version_by_sample_name, fname_out):
     tmp = []
 
     for samplename, fname in zip(samplename_list, fname_list):
@@ -17,7 +43,9 @@ def main(fname_list, samplename_list, fname_out):
                     {
                         "filename": fname,
                         "source": f"{samplename}__{res}",
-                        "genome_assembly": c.info["genome-assembly"],
+                        "genome_assembly": get_genome_version(
+                            samplename, genome_version_by_sample_name, c.info
+                        ),
                         "bin_size": c.binsize,
                     }
                 )
@@ -28,7 +56,9 @@ def main(fname_list, samplename_list, fname_out):
                 {
                     "filename": fname,
                     "source": samplename,
-                    "genome_assembly": c.info["genome-assembly"],
+                    "genome_assembly": get_genome_version(
+                        samplename, genome_version_by_sample_name, c.info
+                    ),
                     "bin_size": c.binsize,
                 }
             )
@@ -40,5 +70,6 @@ if __name__ == "__main__":
     main(
         snakemake.input.fname_list,
         snakemake.params["samplename_list"],
+        snakemake.config.get("genome_versions", {}),
         snakemake.output.fname,
     )
