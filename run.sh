e0388568fe89bff982a9c30df79ec21de00803f3
kpj <kim.philipp.jablonski@gmail.com>
2026-04-17 09:42:31 -0400

    Automate core count selection in run script

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-uv run snakemake --jobs 1 --software-deployment-method conda --resources hdf5_lock=1
+#!/usr/bin/env bash
+
+N_JOBS=$(uv run python -c "import os; print(max(1, (os.cpu_count() or 1) - 2))")
+uv run snakemake --jobs "$N_JOBS" --software-deployment-method conda --resources hdf5_lock=1 "$@"
