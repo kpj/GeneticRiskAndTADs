cd601943e72f051512d471b8f7663826d5fb538b
kpj <kim.philipp.jablonski@gmail.com>
2026-07-20 11:58:53 -0400

    Fix spurious pipeline crashes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 N_JOBS=$(uv run python -c "import os; print(max(1, (os.cpu_count() or 1) - 2))")
-uv run snakemake --jobs "$N_JOBS" --software-deployment-method conda --resources hdf5_lock=1 "$@"
+uv run snakemake --jobs "$N_JOBS" --software-deployment-method conda --resources hdf5_lock=1 notebook_slots=1 "$@"
